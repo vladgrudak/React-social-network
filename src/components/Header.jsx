a5a4314a867ccba8ac9f847d750af20ef4af7b39
vladgrudak <ralkavladislav@gmail.com>
2021-09-23 23:19:05 +0300

    updated App.css, App.js; added Header.jsx, Navbar.jsx, Profile.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://clipart-best.com/img/chrome-logo/chrome-logo-clip-art-32.png'/>
+        </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://c.wallhere.com/photos/d1/bd/3840x2160_px_bridge_sea_sunset-1420607.jpg!d'/>
+            </div>
+            <div>
+                <img src='https://i.pinimg.com/originals/d4/67/f0/d467f0e2caee9eb0fe5acba44877cc17.jpg'/>
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                    <div>
+                        Post3
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Profile;
